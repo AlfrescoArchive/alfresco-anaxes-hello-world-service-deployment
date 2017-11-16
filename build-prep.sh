98da5912003122deb7804c77d5d8408cff8a039c
Ray Gauss II <rgauss@rgauss.com>
2017-11-16 05:30:34 -0500

    DEPLOY-203
    - Attempt to change dependency plugin to fix Bamboo

--- build-prep.sh
+++ build-prep.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 . ./build.properties
 
-mvn org.apache.maven.plugins:maven-dependency-plugin:3.0.2:copy \
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.9:copy \
     -Dartifact=$SERVICE_MAVEN_GROUP_ID:$SERVICE_MAVEN_ARTIFACT_ID:$SERVICE_MAVEN_VERSION \
     -DoutputDirectory=./
